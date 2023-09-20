42eb202cc48864e3a1e77a92755d549428f84003
VortexZAA <abidinayhan94@gmail.com>
2023-09-20 21:17:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    console.log("scanResult", scanResult);
+  }, [scanResult]);
+
   return (
     <div className="App ">
       <h1>QR Code Scanner in React</h1>
@@ -35,21 +39,21 @@ function App() {
         <div
           id="reader"
           style={{
-            maxWidth: "600px",
+            maxWidth: "350px",
             margin: "0 auto",
           }}
         ></div>
         {scanResult && (
-          <div className="scan-result">
+          <div
+            className="scan-result"
+            style={{
+              maxWidth: "350px",
+              margin: "0 auto",
+            }}
+          >
             <h2>Scan Result</h2>
 
-            <p>
-              {scanResult.map((a,index) => (
-                <div key={index}>
-                  <p>{index+1}. - {a}</p>
-                </div>
-              ))}
-            </p>
+            <ResultContainerPlugin results={scanResult} />
           </div>
         )}
         {/* <div>
@@ -62,3 +66,55 @@ function App() {
 }
 
 export default App;
+
+function filterResults(results) {
+  let filteredResults = [];
+  for (var i = 0; i < results.length; ++i) {
+    if (i === 0) {
+      filteredResults.push(results[i]);
+      continue;
+    }
+
+    if (results[i] !== results[i - 1]) {
+      filteredResults.push(results[i]);
+    }
+  }
+  return filteredResults;
+}
+
+const ResultContainerTable = ({ data }) => {
+  const results = filterResults(data);
+  return (
+    <table className={"Qrcode-result-table"}>
+      <thead>
+        <tr>
+          <td>#</td>
+          <td>Decoded Text</td>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((a, i) => {
+          
+          return (
+            <tr key={i}>
+              <td>{i}</td>
+              <td>{a}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const ResultContainerPlugin = (props) => {
+  const results = filterResults(props.results);
+  return (
+    <div className="Result-container">
+      <div className="Result-header">Scanned results ({results.length})</div>
+      <div className="Result-section">
+        <ResultContainerTable data={results} />
+      </div>
+    </div>
+  );
+};
